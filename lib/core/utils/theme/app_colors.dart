ee7871e2e0384eea45e80c8b9db32fa5ef26737d
romanyraafat <romanyraafat2004@gmail.com>
2026-05-08 00:05:09 +0300

    + add app colors class.

---
+++ lib/core/utils/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color gold = Color(0xFFE9C380);
+  static const Color black = Color(0xFF202020);
+  static const Color white = Color(0xFFffffff);
+  static const Color gray = Color(0xFF2D2B2A);
+  static const Color offWhite = Color(0xFFFFF5E3);
+  static const Color brown = Color(0xFF856B3F);
+}
