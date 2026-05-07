f12a2d15c3946cc9c03afd8b3793a88c2cb51151
romanyraafat <romanyraafat2004@gmail.com>
2026-05-08 01:02:29 +0300

    =>finish native splash screen. +handel splash screen for android under 12 & for all other plateform. +handel splash screen for android 12+.

---
+++ lib/core/utils/my_assets/assets.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: prefer_single_quotes
+class MyAsset {
+  MyAsset._();
+  
+  /// Assets for assetsImagesSplashScreen
+  /// assets/images/splash_screen.png
+  static const String assetsImagesSplashScreen = "assets/images/splash_screen.png";
+
+  /// Assets for assetsImagesSplashScreen4x
+  /// assets/images/splash_screen_4x.png
+  static const String assetsImagesSplashScreen4x = "assets/images/splash_screen_4x.png";
+}
+
